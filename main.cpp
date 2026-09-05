213e976dc54e09e5ac4908e9e774cdfdaa7c4d69
jak.u.b.p <130249082+uba1709@users.noreply.github.com>
2026-09-05 15:25:52 +0200

    01

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Obsługa FTXUI
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+using namespace ftxui;
+using namespace std;
+
+
+
+int main() {
+  
+  return 0;
+}
